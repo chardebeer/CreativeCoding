0b6d3c59c47c5a8bf2047d4ca9e82aef512d27d3
Charlize de Beer <chardebeer94@gmail.com>
2021-11-29 19:24:36 +0200

    U3 Experiments

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -21,11 +21,18 @@ return Math.random() * (max - min) + min;
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+
+    const gradient = context.createLinearGradient(0.5, 0.5, 200, 300)
+gradient.addColorStop(0, '#c779d0');
+gradient.addColorStop(1, '#4bc0c8');
+
+
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
 
+ 
 
-    context.fillStyle = 'black';
+    context.fillStyle = gradient;
 const cx = width * 0.5;
 const cy = height * 0.5;
 const w = width * 0.01;
@@ -34,7 +41,7 @@ let x, y;
 
 
 
-const num = 40;
+const num = 36;
 const radius = width * 0.3;
 
 for (let i = 0; i < num; i++  ){
@@ -47,18 +54,21 @@ y = cy + radius * Math.cos(angle);
   context.save();
   context.translate(x, y);
   context.rotate(-angle);
-  context.scale(random.range(0.1, 2), random.range(0.2, 0.5))
+  context.scale(random.range(0.4, 1), random.range(0.3, 1))
 
   context.beginPath();
-  context.rect(-w * 0.5, random.range(0, -h * 0.5),w, h);
+  context.rect(-w * 0.5, random.range(0, -h * 0.3),w, h);
   context.fill();
   context.restore();
 
+
+
+  context.strokeStyle = gradient;
   context.save();
   context.translate(cx, cy);
   context.rotate(-angle);
 
-  context.lineWidth = random.range(5, 20);
+  context.lineWidth = random.range(5, 10);
 
   context.beginPath();
   context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
